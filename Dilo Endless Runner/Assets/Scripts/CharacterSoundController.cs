a3a89b47e6c9493adcf1487c6c03c44e7b6c2d23
raymontaro <mega.ray.mon93@gmail.com>
2021-06-22 15:37:56 +0700

    no message

---
+++ Dilo Endless Runner/Assets/Scripts/CharacterSoundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSoundController : MonoBehaviour
+{
+
+    public AudioClip jump;
+
+    private AudioSource audioPlayer;
+
+    public AudioClip scoreHighlight;
+
+    private void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    public void PlayJump()
+    {
+        audioPlayer.PlayOneShot(jump);
+    }
+
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+}
